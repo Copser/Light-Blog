036622273ca14a0a67896cca3077d17b72d79b3c
Copser <iamrmot@gmail.com>
2014-12-27 07:15:30 +0100

    First commit, starting to make vegan blog post, pamet u glavu!

---
+++ generator.py
@@ -0,0 +1,25 @@
+
+import sys
+
+from django.conf import settings 
+
+
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY = '5y_6z33j1!bk+0+7q3@lbb=bsgh+_ie2^oy6y!(3$30(t=*-t-',
+    ROOT_URLCONF ='vegansite.urls',
+    MIDDLEWARE_CLASSES=(),   
+    INSTALLED_APPS = (
+        'django.contrib.staticfiles',
+        'django.contrib.webdesign',
+        'vegansite',
+    ),
+    STATIC_URL = '/static/',
+)       
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
